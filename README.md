60e598c5dd11a10c03735b08f397508ee99fa9ee
Szymon <bilinskiszymon123@gmail.com>
2023-11-13 22:18:04 +0100

    Working alpha version

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Download-Organizer
+
+simple python script to organize downloads folder. it creates multiple folders in downloads folder and moves files into specific folders depending on file extension.
---
+++ download_organizer.py
@@ -0,0 +1,57 @@
+import os
+import collections
+from pprint import pprint
+
+EXT_AUDIO = ['mp3','wav','raw','wma','mid','midi']
+EXT_VIDEO = ['mp4','mpg','mpeg','avi','mov','flv','mkv','mwv','m4v','h264']
+EXT_IMGS = ['png','jpg','jpeg','gif','svg','bmp','psd','tiff','tif']
+EXT_DOCS = ['txt','pdf','csv','xls','xlsx','ods','doc','docx','html','odt','tex','ppt','pptx','log']
+EXT_COMPR = ['zip','z','7z','rar','tar','gz','rpm','pkg','zip','deb']
+EXT_INSTL = ['dmg','exe','iso']
+
+
+BASE_PATH = os.path.expanduser('~')
+DEST_DIRS = ['Music','Movies','Pictures','Documents','Applications','Other']
+
+
+DOWNLOADS_PATH = os.path.join(BASE_PATH,'Downloads')
+files_mapping = collections.defaultdict(list)
+files_list = os.listdir(DOWNLOADS_PATH)
+
+for d in DEST_DIRS:
+    dir_path = os.path.join(DOWNLOADS_PATH,d)
+    if not os.path.isdir(dir_path):
+        os.mkdir(dir_path)
+
+for file_name in files_list:
+    file_ext = file_name.split('.')[-1]
+    files_mapping[file_ext].append(file_name)
+
+pprint(files_mapping)
+
+for f_ext, f_list in files_mapping.items():
+
+
+    if f_ext in EXT_INSTL:
+        for file in f_list:
+            os.rename(os.path.join(DOWNLOADS_PATH,file), os.path.join(DOWNLOADS_PATH,'Applications',file))
+    elif f_ext in EXT_COMPR:
+        for file in f_list:
+            os.rename(os.path.join(DOWNLOADS_PATH,file), os.path.join(DOWNLOADS_PATH,'Applications',file))
+    elif f_ext in EXT_DOCS:
+        for file in f_list:
+            os.rename(os.path.join(DOWNLOADS_PATH,file), os.path.join(DOWNLOADS_PATH,'Applications',file))
+    elif f_ext in EXT_IMGS:
+        for file in f_list:
+            os.rename(os.path.join(DOWNLOADS_PATH,file), os.path.join(DOWNLOADS_PATH,'Applications',file))
+    elif f_ext in EXT_VIDEO:
+        for file in f_list:
+            os.rename(os.path.join(DOWNLOADS_PATH,file), os.path.join(DOWNLOADS_PATH,'Applications',file))
+    elif f_ext in EXT_AUDIO:
+        for file in f_list:
+            os.rename(os.path.join(DOWNLOADS_PATH,file), os.path.join(DOWNLOADS_PATH,'Applications',file))
+    else:
+        for file in f_list:
+            for file in f_list:
+                os.rename(os.path.join(DOWNLOADS_PATH,file), os.path.join(DOWNLOADS_PATH,'Other',file))
+
